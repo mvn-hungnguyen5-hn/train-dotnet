f6b28400fbaa2302f21ff40782661e96a35a4431
mvn-hungnguyen5-hn <hung.nguyen5@monstar-lab.com>
2022-08-16 17:14:28 +0700

    Tuan1-Bai6-7

---
+++ Tuan1/Bai6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+namespace Bai6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("----------Chương trình tính tổng các số nguyên dương từ 0-n---------");
+            Console.WriteLine("Nhập số nguyên dương n");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i <= number; i++)
+            {
+                sum+=i;
+                
+            }
+            Console.WriteLine($"Tổng: {sum}");
+
+        }
+    }
+}
---
+++ Tuan1/Bai7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+namespace Bai6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("----------Chương trình tính các phép nhân---------");
+            Console.WriteLine("Nhập số nguyên dương n");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int tich = 0;
+            for (int i = 1; i <= 20; i++)
+            {
+                tich = number*i;
+                Console.WriteLine($"Tích {number}x{i}: {tich}");
+                
+            }
+        }
+    }
+}
