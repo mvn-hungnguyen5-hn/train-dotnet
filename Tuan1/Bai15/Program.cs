53405da3d41e7d939d821ef8bfee7b07ddbda82a
Hung Nguyen B.[5] VN.Hanoi <104880717+mvn-hungnguyen5-hn@users.noreply.github.com>
2022-08-18 19:49:01 +0700

    test cf

--- Tuan1/Bai15/Program.cs
+++ Tuan1/Bai15/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Conflict chết mất tiêu");
 Console.WriteLine("Hello, World!");
 
