d3301a065b784c7d18557ce5f0fdd8e5b3396b7c
mvn-hungnguyen5-hn <hung.nguyen5@monstar-lab.com>
2022-08-18 19:58:19 +0700

    test

--- Tuan1/Bai15/Program.cs
+++ Tuan1/Bai15/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Conflict chết mất tiêu");
+Console.Write("Conflict chết mất tiêu thât");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Test");
 
