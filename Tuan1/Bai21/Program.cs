630cb9747324d2008f00f263ebbe2020d5617cfa
mvn-hungnguyen5-hn <hung.nguyen5@monstar-lab.com>
2022-08-20 11:23:04 +0700

    "test conflict"

---
+++ Tuan1/Bai21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Bai21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Chương trình kiểm tra định dạng chuỗi nhập vào");
+            Console.WriteLine("Nhập chuối số:");
+        }
+    }
+}
